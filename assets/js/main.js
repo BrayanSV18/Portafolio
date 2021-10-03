7b801022fb67d818da4296157beff6287dc7aa38
BrayanSV18 <brayanvenegas24@gmail.com>
2021-10-03 13:32:01 -0600

    edit index and create ligthbox file

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,6 @@ if (navToggle){
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', () =>{
         nav__menu.classList.remove('show-menu')
@@ -20,7 +19,14 @@ if(navClose){
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
